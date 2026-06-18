ddb5f20b44c71b643c19697fb1c09c147247b2bb
SANTHIYA-1431 <rsanthiya117@gmail.com>
2026-06-18 23:35:08 +0530

    learned concatenation topic

---
+++ STRING/concatenation.java
@@ -0,0 +1,13 @@
+/*Concatenation is the process of combining two or more strings (or values) 
+into a single string using the + operator*/
+class concatenation{
+    public static void main(String[] args) {
+        String a1="java-";
+        String a2="virtual-";
+        String a3=a1+a2;
+        System.out.println(a3);
+        String a4="machine";
+        String a5=a3+a4;
+        System.out.println(a5);
+    }
+}
