8e6814231cd7b4c2c7b70746cba8e65dcbf5890b
SANTHIYA-1431 <rsanthiya117@gmail.com>
2026-06-20 22:38:40 +0530

    learned without arguments

---
+++ METHODS/arguments/WithoutArguments.java
@@ -0,0 +1,12 @@
+public class WithoutArguments
+{
+    static void withoutArguments()   // method without arguments
+    {
+        System.out.println("java full development");
+    }
+
+    public static void main(String[] args)
+    {
+        withoutArguments();
+    }
+}
