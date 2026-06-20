7f38643c8a71ae934382561fb44b6c3f1d770406
SANTHIYA-1431 <rsanthiya117@gmail.com>
2026-06-20 22:22:24 +0530

    function

---
+++ METHODS/function.java
@@ -0,0 +1,10 @@
+// A function (method in Java) is a block of code that performs a specific task and can be reused.
+
+public class function {
+    static void good(){
+        System.out.println("hello sandy");
+    }
+    public static void main(String[] args){
+        good();
+    }
+}
