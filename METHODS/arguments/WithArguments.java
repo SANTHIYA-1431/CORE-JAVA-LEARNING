9febe60acbcb36a18c0c2f8e69141cdb43e5e5e3
SANTHIYA-1431 <rsanthiya117@gmail.com>
2026-06-20 22:41:35 +0530

    learned witharguments

---
+++ METHODS/arguments/WithArguments.java
@@ -0,0 +1,12 @@
+public class WithArguments
+{
+    static void display(String course)
+    {
+        System.out.println(course);
+    }
+
+    public static void main(String[] args)
+    {
+        display("Java Full Development");
+    }
+}
