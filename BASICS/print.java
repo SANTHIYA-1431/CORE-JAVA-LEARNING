9912c42dca25a2598583ac296d4001cd15928c20
SANTHIYA-1431 <rsanthiya117@gmail.com>
2026-06-22 21:20:36 +0530

    learned print function in java

---
+++ BASICS/print.java
@@ -0,0 +1,9 @@
+/* print → write continuously in the same line
+   println → write and move to the next line   */
+
+public class print {
+    public static void main(String[] args) {
+        System.out.print("prints in same lines until \n used " );
+        System.out.println("prints in new line");
+    }
+}
