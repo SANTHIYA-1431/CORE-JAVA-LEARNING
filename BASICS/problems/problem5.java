e2c67bd9b838dc14a0fccff8f5c6195d6fc35f4e
SANTHIYA-1431 <rsanthiya117@gmail.com>
2026-06-26 23:58:31 +0530

    solved problem 16

--- BASICS/problems/problem5.java
+++ BASICS/problems/problem5.java
@@ -1,23 +1,11 @@
-// Calculate total and average of 5 subject marks
-import java.util.Scanner;
-public class problem5 {
- public static void main (String args []){
-    Scanner sc = new Scanner(System.in);
-    System.out.println ("enter first subject marks");
-    int mark1 =sc.nextInt ();
-        System.out.println ("enter second subject marks");
-         int mark2 =sc.nextInt ();
-        System.out.println ("enter third subject marks");
-         int mark3 =sc.nextInt ();
-         System.out.println ("enter fourth subject marks");
-          int mark4 =sc.nextInt ();
-         System.out.println ("enter fifth subject marks");
-          int mark5 =sc.nextInt ();
-          int total = mark1+mark2+mark3+mark4+mark5;
-          System.out.println("total marks is : "+total);
-          int average = ( mark1+mark2+mark3+mark4+mark5)/5;
-          System.err.println("average marks is "+average);
-                   
+// 	Write a Java program to calculate simple interest.
 
- }   
+public class problem6 {
+    public static void main(String[] arge){
+        double principal = 1000;
+        double rate = 5;
+        double time = 3;
+        double simpleInterest = (principal * rate * time) / 100;
+        System.out.println("Simple Interest: " + simpleInterest);
+    } 
 }
